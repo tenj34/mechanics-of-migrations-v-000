653178bbb9b66c680f2ee2726df4fcd2cf5c3959
Juan Tenezaca <tenj34@gmail.com>
2018-10-01 22:48:43 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,4 +4,13 @@ class CreateArtists < ActiveRecord::Migration
 
   def down
   end
+
+  def change
+  create_table :artists do |t|
+    t.string :name
+    t.string :genre
+    t.integer :age
+    t.string :hometown
+  end
+end
 end
